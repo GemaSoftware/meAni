6f2a24fa1e5ff15f6b8ac53c6630df79e907a542
Agron Gemajli <agrongemajli@outlook.com>
2021-02-19 20:24:30 -0500

    Initial Commit

---
+++ meAni/meAniApp.swift
@@ -0,0 +1,17 @@
+//
+//  meAniApp.swift
+//  meAni
+//
+//  Created by Agron Gemajli on 2/19/21.
+//
+
+import SwiftUI
+
+@main
+struct meAniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
